fc6718301b12f99e9f66503699fe9a363fb80d1b
lramosduarte <lramosduarte@gmail.com>
2015-11-27 18:04:47 -0200

    verificar o post no server

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,31 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({  
     model() {
-        /*let todos = [
-            {
-                descricao: "Learn Ember",
-                concluido: false,
-            },
-            {
-                descricao: "Solve World Hunger",
-                concluido: false,
-            }
-        ];
-        return todos;*/
         return this.store.findAll('todo');
     },
     actions: {
-        createTodo(novoItem) {
-           this.store.createRecord('todo', {
-               descricao: novoItem,
-               concluido: false
-           }).save();
+        createTodo(novoTitulo) {
+            this.store.createRecord('todo', {
+                descricao: novoTitulo,
+                concluido: false
+            }).save();
         },
-        updateTodo(todo){
-            todo.save();
-        },
-        deleteTodo(todo){
-            todo.destroyRecord();
-        }
+       updateTodo(todo) {
+           todo.save();
+       },
+       deleteTodo(todo) {
+           todo.destroyRecord();
+       }
     }
 });
