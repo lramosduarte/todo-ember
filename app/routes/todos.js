1fd1a17f3990a2e5408f80556f1d9898b4a46a7f
lramosduarte <lramosduarte@gmail.com>
2015-12-03 10:37:20 -0200

    carregando multiplos modelos em uma rota e exibindo

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({  
     model() {
-        /*return Ember.RSVP.hash({
+        return Ember.RSVP.hash({
           todo: this.store.findAll('todo'),
-          comentarios: this.store.findAll('comentario')
-        });*/
-        return this.store.findAll('todo');
+          comentarios: this.store.findAll('comentario'),
+        });
+        //return this.store.findAll('todo');
     },
-    /*setupController(controller, models){
+    setupController(controller, models){
         controller.set('todos', models.todo);
-        controller.set('comentarios', models.comentario);
-    },*/
+        //controller.set('comentarios', models.comentario);
+    },
     actions: {
         createTodo(novoTitulo) {
             let todo = this.store.createRecord('todo', {
--- app/routes/todos/index.js
+++ app/routes/todos/index.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({  
     model() {
-        return this.modelFor('todos');
+        return this.store.findAll('todo');
+    },
+    renderTemplate(controller, model) {
+        this.render('todos.index', {
+            model: model
+        });
     }
 });
