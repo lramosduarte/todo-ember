27d0b7aeddaaaa4cc8bc535599fe5a3da7971f73
lramosduarte <lramosduarte@gmail.com>
2015-12-04 10:28:21 -0200

    mesma coisa

--- app/routes/todos/comentarios.js
+++ app/routes/todos/comentarios.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model(params){
 		console.log(params)
-		return this.store.find('comentario', params.tarefas_id);
+		//return this.store.find('comentario', params.tarefas_id);
 	}
 });
